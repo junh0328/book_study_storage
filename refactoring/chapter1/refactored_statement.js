90d1995b6d338e1bca308776f9819443e2431895
junh0328 <junh0328@naver.com>
2022-06-25 21:50:22 +0900

    refactor: statement() 함수 쪼개기 - amountFor 함수로 분리하기

---
+++ refactoring/chapter1/refactored_statement.js
@@ -0,0 +1,95 @@
+// Before refactoring
+let invoices = [
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  },
+];
+
+let plays = {
+  hamlet: {
+    name: "hamlet",
+    type: "tragedy",
+  },
+  "as-like": {
+    name: "As You Like It",
+    type: "comedy",
+  },
+  othello: {
+    name: "Othello",
+    type: "tragedy",
+  },
+};
+
+// amountFor 함수 분리하기
+function amountFor(perf, play) {
+  let result = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      result = 40000;
+      if (perf.audience > 30) {
+        result += 1000 * (perf.audience - 30);
+      }
+      break;
+
+    case "comedy":
+      result = 30000;
+      if (perf.audience > 20) {
+        result += 10000 + 500 * (perf.audience - 20);
+      }
+      result += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+
+  return result;
+}
+
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+
+    let thisAmount = amountFor(perf, play);
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += `${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+invoices.map((invoice) => console.log(statement(invoice, plays)));
