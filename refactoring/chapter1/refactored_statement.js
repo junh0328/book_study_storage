7e992f5c5c8c1ea164636480f53dcd0ff9443643
junh0328 <junh0328@naver.com>
2022-06-25 22:43:38 +0900

    refactor: 반복문 쪼개기, 임시 변수를 질의 함수로 바꾸기 - 함수 추출하기

--- refactoring/chapter1/refactored_statement.js
+++ refactoring/chapter1/refactored_statement.js
@@ -82,16 +82,22 @@ function usd(aNumber) {
 }
 
 function totalVolumeCredits(invoice) {
-  let volumeCredits = 0;
+  let result = 0;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
+    result += volumeCreditsFor(perf);
   }
-  return volumeCredits;
+  return result;
+}
+
+function appleSauce(invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
+  }
+  return result;
 }
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
-
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -99,10 +105,9 @@ function statement(invoice, plays) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(appleSauce(invoice))}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
   return result;
 }
