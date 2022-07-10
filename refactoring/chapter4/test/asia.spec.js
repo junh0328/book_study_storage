b1f33bb113ac88420e6b187d972ef33cc6465040
junh0328 <junh0328@naver.com>
2022-07-10 12:40:06 +0900

    refactor: 재사용 가능한 로컬 변수(asia) 빼내기

--- refactoring/chapter4/test/asia.spec.js
+++ refactoring/chapter4/test/asia.spec.js
@@ -2,14 +2,16 @@ const { expect } = require("chai");
 const { Province, sampleProvinceData } = require("../sample");
 
 describe("province", function () {
+  let asia;
+  this.beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     // expect(asia.shortfall, 5);
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
